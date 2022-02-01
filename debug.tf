b26baeba9e4b343de952344b3eb0d94edb6fa46e
Nicholas Santiago <nicholas@santiago.wtf>
2022-01-31 21:17:03 -0500

    getting ready for 0.0.2

---
+++ debug.tf
@@ -0,0 +1,10 @@
+# output "domains" {
+#   description = "For debug use - list of domains"
+#   value = {
+#     domain_list      = local.domain_list
+#     domain_list_full = local.domain_list_full
+#     primary_domain   = local.primary_domain
+#     san_list         = local.san_list
+#     s3_origin_id     = local.s3_origin_id
+#   }
+# }
